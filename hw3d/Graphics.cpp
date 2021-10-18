6fd06312ef8f10793d302f32cc1fd55517f1b441
Pzh <1057721283@qq.com>
2021-10-18 16:56:13 +0800

    总结:各种Primitive Topology 类型的具体含义

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -162,8 +162,6 @@ void Graphics::DrawTestTriangle()
 	{
 		{0.0f, 0.5f},
 		{0.5f, -0.5f},
-		{0.5f, -0.5f},
-		{-0.5f, -0.5f},
 		{-0.5f, -0.5f},
 		{0.0f, 0.5f},
 	};
@@ -280,8 +278,9 @@ void Graphics::DrawTestTriangle()
 	
 	// ————Primitive Topology 原始拓扑
 	/* 描述IA阶段传入的点之间的拓扑关系，即确定图元  
-	 比如输入0 1 2 3 4 5 点。线：01 23 45三条线。 三角形：012 345。 三角形带:012 123 234 345 */
-	pContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST); 
+	 比如输入0 1 2 3 4 5 点。LINELIST：01 23 45三条线。 TRIANGLELIST：012 345。 TRIANGLESTRIP:012 123 234 345 
+	 LINESTRIP: 01 12 23 34 45*/
+	pContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINESTRIP);  
 
 
 
